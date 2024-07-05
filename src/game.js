11f272789dcbfc1fd2a56a56f59d0669e3f9d76b
Inoue <162900460+onigiri860@users.noreply.github.com>
2024-07-06 06:32:20 +0900

    Add files via upload

---
+++ src/game.js
@@ -0,0 +1,18 @@
+// キャラクターをインスタンス化する
+let hero = new Hero("シェフ", 200, 50, 20, 3, 30, "C:/Battle_js/command_battle/img/chef.png");  // 主人公
+let enemy = new Fish("さかな", 150, 40, 10, "C:/Battle_js/command_battle/img/sakana.png");      // 敵
+
+// キャラクター配列をつくる
+let characters = [];
+characters.push(hero);     // 主人公
+characters.push(enemy);      // 敵
+
+
+// ゲーム管理クラスをインスタンス化する
+let gameManage = new GameManage();
+
+// コマンドクラスをインスタンス化する
+let command = new Command();
+
+// コマンド選択の準備を整える
+command.preparation();
